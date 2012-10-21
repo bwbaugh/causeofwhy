c0d54cb9536618a385503e527b8d5bcaedd78bad
B. W. Baugh <brian@brianbaugh.com>
2012-10-21 03:49:04 -0500

    Add: page_list function to display list of Pages
    Mainly used for testing at this point, will display a list of Page IDs
    and titles of the page argument (can be iterable). Good for seeing the
    results of a index.intersect() call.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # Copyright (C) 2012 Brian Wesley Baugh
 """Main program to start the Cause of Why QA system."""
 import sys
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
 
 from causeofwhy import indexer
 
@@ -11,6 +15,22 @@ from causeofwhy import indexer
 # wikilocation = 'C:/wiki/enwiki-20120902-pages-articles-multistream.xml'
 wiki_location = 'R:/_Other/Wikipedia/simplewiki-20121002-pages-articles.xml'
 # wiki_location = 'C:/wiki/simplewiki-20121002-pages-articles.xml'
+
+
+def page_list(page):
+    try:
+        iterator = iter(page)
+    except TypeError:
+        page.unidecode()
+        return ' '.join([str(page.ID), page.title])
+    else:
+        f = StringIO()
+        for page in iterator:
+            page.unidecode()
+            f.write(' '.join([str(page.ID), page.title]) + '\n')
+        output = f.getvalue().rstrip()
+        f.close()
+        return output
 
 
 def main():
