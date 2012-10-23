30aca3b29ad521dc0f6cb24f9b98bce22e1ad61d
B. W. Baugh <brian@brianbaugh.com>
2012-10-23 06:22:14 -0500

    ADD: web UI now handles requests asynchronously
    Before we handled each user's request all at once. However, this causes
    the main Tornado IO loop to block meaning it cannot handle ANY other
    requests!
    
    By using the apply_async() method of multiprocessing pool, we are able
    to hand off the CPU intensive portion of the work to a different process
    and wait for the results. In the mean time, we can handle requests from
    other users.
    
    Future work would be to parallelize the CPU intensive portion of the
    work, which would result in the average response time per request to
    become shorter. Currently we spawn P processes, which means we can
    handle up to P users and their queries at once. However, it might be
    better to have less of these handler processes and more worker processes
    that divide up the data.

--- causeofwhy/web.py
+++ causeofwhy/web.py
@@ -1,11 +1,17 @@
 # Copyright (C) 2012 Brian Wesley Baugh
 """Web interface allowing users to submit queries and get a response."""
 import os
+import multiprocessing
+
 import tornado.ioloop
 import tornado.web
 import tornado.httpserver
 
 from causeofwhy import indexer
+
+
+# How many page worker threads to use
+NUMBER_OF_PROCESSES = max(1, multiprocessing.cpu_count() - 1)
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -14,9 +20,11 @@ class MainHandler(tornado.web.RequestHandler):
 
 
 class QueryHandler(tornado.web.RequestHandler):
-    def initialize(self, index):
-        self.index = index
+    def initialize(self):
+        self.index = self.application.settings.get('index')
+        self.pool = self.application.settings.get('pool')
 
+    @tornado.web.asynchronous
     def get(self):
         query = self.get_argument('query')
         num_top = int(self.get_argument('top', default=10))
@@ -30,6 +38,20 @@ class QueryHandler(tornado.web.RequestHandler):
         answers, similarity = zip(*answers)
         # Retrieve the Page objects from the list of Page.IDs
         answers = self.index.get_page(answers)
+        args = (answers, similarity, query, ir_query, num_results, answers)
+        self.pool.apply_async(handle_answers, (args,), callback=self.callback)
+
+    def callback(self, result):
+        query, ir_query, num_results, answers = result
+        self.render("answer.html",
+                    query=query,
+                    ir_query=' '.join(ir_query),
+                    num_results=num_results,
+                    answers=answers)
+
+
+def handle_answers(args):
+        answers, similarity, query, ir_query, num_results, answers = args
         for answer, sim in zip(answers, similarity):
             answer.cosine_sim = sim
             answer.preprocess()
@@ -41,19 +63,18 @@ class QueryHandler(tornado.web.RequestHandler):
                        in ir_query)):
                     sentences.append(sentence)
             answer.sentences = sentences
-        self.render("answer.html",
-                    query=query,
-                    ir_query=' '.join(ir_query),
-                    num_results=num_results,
-                    answers=answers)
+        return query, ir_query, num_results, answers
 
 
 def main(index):
+    pool = multiprocessing.Pool(NUMBER_OF_PROCESSES)
     application = tornado.web.Application([
         (r"/", MainHandler),
-        (r"/cause/", QueryHandler, dict(index=index)),
+        (r"/cause/", QueryHandler),
         ], template_path=os.path.join(os.path.dirname(__file__), "templates"),
-        static_path=os.path.join(os.path.dirname(__file__), "static"))
+        static_path=os.path.join(os.path.dirname(__file__), "static"),
+        index=index,
+        pool=pool)
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(8080)
     tornado.ioloop.IOLoop.instance().start()
