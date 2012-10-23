eb2bc3d6a928aca737b1397d691f8ecc5922e053
B. W. Baugh <brian@brianbaugh.com>
2012-10-23 17:10:56 -0500

    Task: make lib folder import more readable

--- causeofwhy/__init__.py
+++ causeofwhy/__init__.py
@@ -9,7 +9,7 @@ import inspect
 # they will be used even if they were not installed into Python's
 # site-packages/ directory.
 # Note: the lib_dir var is still available if needed from the outside.
-lib_dir = os.path.realpath(os.path.abspath(os.path.join(os.path.split(
-    inspect.getfile(inspect.currentframe()))[0], "../lib")))
+module_path = os.path.split(inspect.getfile(inspect.currentframe()))[0]
+lib_dir = os.path.realpath(os.path.abspath(os.path.join(module_path, "../lib")))
 if lib_dir not in sys.path:
     sys.path.insert(0, lib_dir)
